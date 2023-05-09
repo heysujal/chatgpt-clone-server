01619df8a594627951c8f2bd3a8f274cf4e511fd
Sujal Gupta <sujalgupta6100@gmail.com>
2023-05-09 22:28:37 +0530

    add route for image generation
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- server.js
+++ server.js
@@ -31,6 +31,28 @@ app.post('/completions', async (req, res)=>{
         console.log(error);
     }
 })
+app.post('/generateimages', async (req, res)=>{
+    // console.log(req.body);
+    try {
+        
+        let response = await fetch('https://api.openai.com/v1/images/generations', {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json',
+                'Authorization' : `Bearer ${OPENAI_API_KEY}`
+            },
+            body : JSON.stringify({
+                n: 5,
+                prompt: req.body.prompt || 'A Flying Cat',
+                size: '512x512'  
+            })
+        });
+        let data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`App listening on port ${PORT}`)
